da3d9e4ef69fd7a641f940c5c7c57348c67f4e1a
MinoThauros <herimino@timatech.ca>
2023-11-08 11:50:14 -0500

    added get request on /login

--- node-express-mongo/routes/authRouter.js
+++ node-express-mongo/routes/authRouter.js
@@ -4,6 +4,11 @@ const AuthRouter = express.Router()
 AuthRouter.route('/')
 
 .post((req,res,next)=>{
-    const {email,password}=req.params.body
+    const {email,password}=req.body
     res.send(`logging in ${email}`)
+})
+
+.get('/:userId',(req,res,next)=>{
+    const {userId}=req.params
+    res.send(`getting ${userId}`)
 })
